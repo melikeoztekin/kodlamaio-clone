e0656319dd600ea87df57a53b024655d91497428
melikeoztekin <melikeoztekin06@gmail.com>
2022-07-27 15:39:17 +0300

    get data with input directive

--- src/app/components/lesson-card/lesson-card.component.ts
+++ src/app/components/lesson-card/lesson-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-lesson-card',
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lesson-card.component.css']
 })
 export class LessonCardComponent implements OnInit {
-
+  @Input() lesson:any;
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.lesson)
   }
-
+  
 }
--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -3,13 +3,57 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.css']
+  styleUrls: ['./homepage.component.css'],
 })
 export class HomepageComponent implements OnInit {
-
-  constructor() { }
+  lessonList!: any[];
+  constructor() {}
 
   ngOnInit(): void {
+    this.fillLessonData();
   }
-
+  fillLessonData() {
+    this.lessonList = [
+      {
+        id: 1,
+        lessonImg: 'assets/img/lesson1.jpg',
+        lessonName: 'Yazılım Geliştirici Yetiştirme Kampı (JavaScript)',
+        lessonDescription:
+          '1,5 ay sürecek Yazılım Geliştirici Yetiştirme Kampımızın takip, döküman ve duyurularını buradan yapacağız.',
+        instructor: 'engin demiroğ',
+        instructorProfile: 'assets/img/engin-demirog.jpg',
+        price: 'ücretsiz',
+      },
+      {
+        id: 2,
+        lessonImg: 'assets/img/lesson2.jpg',
+        lessonName: 'Yazılım Geliştirici Yetiştirme Kampı (C# + ANGULAR)',
+        lessonDescription:
+          '2 ay sürecek Yazılım Geliştirici Yetiştirme Kampımızın takip, döküman ve duyurularını buradan yapacağız.',
+        instructor: 'engin demiroğ',
+        instructorProfile: 'assets/img/engin-demirog.jpg',
+        price: 'ücretsiz',
+      },
+      {
+        id: 3,
+        lessonImg: 'assets/img/lesson3.jpg',
+        lessonName: 'Yazılım Geliştirici Yetiştirme Kampı (JAVA + REACT)',
+        lessonDescription:
+          '2 ay sürecek Yazılım Geliştirici Yetiştirme Kampımızın takip, döküman ve duyurularını buradan yapacağız.',
+        instructor: 'engin demiroğ',
+        instructorProfile: 'assets/img/engin-demirog.jpg',
+        price: 'ücretsiz',
+      },
+      {
+        id: 4,
+        lessonImg: 'assets/img/lesson4.jpg',
+        lessonName: 'Programlamaya Giriş için Temel Kurs',
+        lessonDescription:
+          'PYTHON, JAVA, C# gibi tüm programlama dilleri için temel programlama mantığını anlaşılır örneklerle öğrenin.',
+        instructor: 'engin demiroğ',
+        instructorProfile: 'assets/img/engin-demirog.jpg',
+        price: 'ücretsiz',
+      },
+    ];
+  }
 }
